b494f8d5e4a5489123c7a2461a22927b30166ab4
Qi Gao <qi.gao2012@gmail.com>
2021-10-12 13:51:33 +0800

    feat: update wiremock - [x] use wiremock junit5 - [x] refactor base wiremock setup - [x] update integrationTest setup - [x] update wire-mock package

--- springboot/wire-mock/src/main/java/com/github/durex/Application.java
+++ springboot/wire-mock/src/main/java/com/github/durex/Application.java
@@ -1,11 +1,12 @@
-package com.github.durex.todo;
+package com.github.durex;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+  @Generated
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
