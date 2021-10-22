8d5e6af87bb52ac45342787ffbbc2431832cc34b
Qi Gao <qi.gao2012@gmail.com>
2021-10-22 13:10:34 +0800

    fmt: format code

--- quarkus/starter/src/test/java/com/github/durex/demo/DemoResourceTest.java
+++ quarkus/starter/src/test/java/com/github/durex/demo/DemoResourceTest.java
@@ -1,10 +1,10 @@
 package com.github.durex.demo;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 class DemoResourceTest {
