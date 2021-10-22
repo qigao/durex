3542350bab52108b2884a9ca975db6c52210dbd3
Qi Gao <qi.gao2012@gmail.com>
2021-10-22 13:00:08 +0800

    feat: apply env vars from .env file - [x] set/unset environment variables from .env - [x] minor update of code - [x] use asciidoc instead of markdown for README - [x] add scripts usage to README.adoc

--- quarkus/starter/src/test/java/com/github/durex/demo/DemoResourceTest.java
+++ quarkus/starter/src/test/java/com/github/durex/demo/DemoResourceTest.java
@@ -1,16 +1,16 @@
 package com.github.durex.demo;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class DemoResourceTest {
 
   @Test
-  public void testHelloEndpoint() {
+  void testHelloEndpoint() {
     given().when().get("/hello").then().statusCode(200).body(is("Hello RESTEasy"));
   }
 }
