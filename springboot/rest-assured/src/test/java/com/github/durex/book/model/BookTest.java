48f0d03df6f4417e38a9f1ede23aaa7e6c2e5690
Qi Gao <qi.gao2012@gmail.com>
2021-10-23 23:19:34 +0800

    Update hash equals test (#27)
    * feat: update hash and equals for POJO
    - [x] introduce EqualsVerifier for testing hash and equals
    - [x] drop sonar job as it spend too much time

--- springboot/rest-assured/src/test/java/com/github/durex/book/model/BookTest.java
+++ springboot/rest-assured/src/test/java/com/github/durex/book/model/BookTest.java
@@ -1,23 +1,11 @@
 package com.github.durex.book.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import com.github.durex.book.support.MockData;
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 
 class BookTest {
   @Test
-  void testEqualsBook() {
-    var book1 = MockData.givenBooks();
-    var book2 = MockData.givenBooks();
-    var book3 = MockData.buildBookRequest();
-    assertEquals(book1, book1);
-    Book nullBook = null;
-    assertNotEquals(book1, nullBook);
-    assertNotEquals(book1, book3);
-    assertTrue(book1.equals(book2) && book2.equals(book1));
-    assertEquals(book1.hashCode(), book2.hashCode());
+  void testEqualBook() {
+    EqualsVerifier.simple().forClass(Book.class).verify();
   }
 }
