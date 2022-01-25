7be14205565193e0b2fbe7bc58328b0c27f2ad32
Qi Gao <qi.gao2012@gmail.com>
2022-01-25 14:44:50 +0800

    Merge pull request #53
    * fix(all): minor fix

--- core/springboot/wire-mock/src/it/java/com/github/durex/todo/TodoControllerTest.java
+++ core/springboot/wire-mock/src/it/java/com/github/durex/todo/TodoControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-class TodoControllerIT extends BaseWireMock {
+class TodoControllerTest extends BaseWireMock {
   @Autowired private WebTestClient webTestClient;
 
   @Test
