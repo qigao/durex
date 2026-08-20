0e4e35f852e9bf55a627c5eeea480cc0defd2f9d
Qi Gao <qi.gao2012@gmail.com>
2026-08-20 19:22:35 +0800

    feat: complete Spring music CRUD adapter

--- core/music/src/spring/java/com/github/durex/music/spring/MusicHttpController.java
+++ core/music/src/spring/java/com/github/durex/music/spring/MusicHttpController.java
@@ -4,9 +4,15 @@ import com.github.durex.music.model.Music;
 import com.github.durex.music.service.MusicService;
 import com.github.durex.shared.model.RespData;
 import com.github.durex.shared.support.Helper;
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +24,31 @@ public class MusicHttpController {
     this.musicService = musicService;
   }
 
+  @GetMapping({"", "/"})
+  public RespData<List<Music>> getMusic(
+      @RequestParam(value = "title", required = false) String title,
+      @RequestParam(value = "id", required = false) String musicId,
+      @RequestParam(value = "offset", defaultValue = "10") int offset) {
+    return RespData.of(musicService.getMusicsByTitle(title), Helper.okResponse());
+  }
+
   @GetMapping("/{id}")
   public RespData<Music> getMusic(@PathVariable("id") String musicId) {
     return RespData.of(musicService.getMusicById(musicId), Helper.okResponse());
+  }
+
+  @PostMapping({"", "/"})
+  public RespData<Integer> createMusic(@RequestBody Music music) {
+    return RespData.of(musicService.createMusic(music), Helper.okResponse());
+  }
+
+  @PutMapping({"", "/"})
+  public RespData<Integer> updateMusic(@RequestBody Music music) {
+    return RespData.of(musicService.updateMusic(music), Helper.okResponse());
+  }
+
+  @DeleteMapping("/{id}")
+  public RespData<Integer> deleteMusic(@PathVariable("id") String musicId) {
+    return RespData.of(musicService.deleteMusicById(musicId), Helper.okResponse());
   }
 }
