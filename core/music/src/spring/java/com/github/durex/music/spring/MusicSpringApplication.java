6af304030aced07c6f174f7b7083be105bce68ab
Qi Gao <qi.gao2012@gmail.com>
2026-08-20 18:40:07 +0800

    feat: wire Spring music runtime with jOOQ

---
+++ core/music/src/spring/java/com/github/durex/music/spring/MusicSpringApplication.java
@@ -0,0 +1,12 @@
+package com.github.durex.music.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MusicSpringApplication.class, args);
+  }
+}
