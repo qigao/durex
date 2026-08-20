76f2f14ef7beefa6b1855471fd5b1340a112dee8
Qi Gao <qi.gao2012@gmail.com>
2026-08-20 23:59:45 +0800

    feat: install Durex capability kernel

--- build-logic/src/main/groovy/com/github/durex/gradle/DurexModulePlugin.groovy
+++ build-logic/src/main/groovy/com/github/durex/gradle/DurexModulePlugin.groovy
@@ -1,5 +1,10 @@
 package com.github.durex.gradle
 
+import com.github.durex.gradle.capability.BuiltinCapabilities
+import com.github.durex.gradle.capability.CapabilityEngine
+import com.github.durex.gradle.capability.CapabilityPluginRegistry
+import com.github.durex.gradle.capability.CapabilityRegistry
+import com.github.durex.gradle.catalog.DependencyCatalogSnapshot
 import com.github.durex.gradle.model.DurexModuleModel
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -9,9 +14,19 @@ class DurexModulePlugin implements Plugin<Project> {
     void apply(Project project) {
         project.pluginManager.apply('durex.catalog')
 
+        DependencyCatalogSnapshot catalog = project.extensions.getByType(DependencyCatalogSnapshot)
         DurexModuleModel model = project.extensions.create('durexModuleModel', DurexModuleModel)
         model.capabilities.convention(Collections.emptySet())
         model.platformBindings.convention(Collections.emptySet())
+
+        CapabilityRegistry capabilityRegistry = new CapabilityRegistry()
+        CapabilityPluginRegistry pluginRegistry = new CapabilityPluginRegistry()
+        CapabilityEngine capabilityEngine = new CapabilityEngine(
+                project, model, catalog, capabilityRegistry, pluginRegistry)
+        project.extensions.add(CapabilityRegistry, 'durexCapabilityRegistry', capabilityRegistry)
+        project.extensions.add(CapabilityPluginRegistry, 'durexCapabilityPluginRegistry', pluginRegistry)
+        project.extensions.add(CapabilityEngine, 'durexCapabilityEngine', capabilityEngine)
+        BuiltinCapabilities.registerAll(capabilityRegistry)
 
         project.extensions.create('durex', DurexExtension, project, model)
 
