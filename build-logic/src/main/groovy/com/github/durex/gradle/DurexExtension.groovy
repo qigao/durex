b7f3468bb24e5c96f20a57b633b3fc9f2b9fdf91
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 00:03:02 +0800

    feat: add generic Durex capability DSL

--- build-logic/src/main/groovy/com/github/durex/gradle/DurexExtension.groovy
+++ build-logic/src/main/groovy/com/github/durex/gradle/DurexExtension.groovy
@@ -1,9 +1,12 @@
 package com.github.durex.gradle
 
+import com.github.durex.gradle.capability.CapabilityEngine
+import com.github.durex.gradle.capability.CapabilityPluginRegistry
 import com.github.durex.gradle.dependency.DependencyBridge
 import com.github.durex.gradle.model.DurexModuleModel
 import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.plugins.UnknownPluginException
 
 class DurexExtension {
     private final Project project
@@ -22,6 +25,30 @@ class DurexExtension {
 
     void dependency(String configuration, String alias) {
         DependencyBridge.add(project, model, configuration, alias)
+    }
+
+    void capability(String pluginId) {
+        try {
+            project.pluginManager.apply(pluginId)
+        } catch (UnknownPluginException e) {
+            throw new DurexConfigurationException(
+                    'Durex configuration error\n' +
+                    "Project: ${project.path}\n" +
+                    "Gradle plugin id: ${pluginId}\n" +
+                    'Problem: capability plugin could not be resolved',
+                    e)
+        }
+
+        CapabilityPluginRegistry pluginRegistry = project.extensions.getByType(CapabilityPluginRegistry)
+        String capabilityId = pluginRegistry.capabilityForPlugin(pluginId)
+        if (capabilityId == null) {
+            throw new DurexConfigurationException(
+                    'Durex configuration error\n' +
+                    "Project: ${project.path}\n" +
+                    "Gradle plugin id: ${pluginId}\n" +
+                    'Problem: plugin did not register a Durex primary capability')
+        }
+        project.extensions.getByType(CapabilityEngine).enable(capabilityId)
     }
 
     PersistenceExtension getPersistence() {
