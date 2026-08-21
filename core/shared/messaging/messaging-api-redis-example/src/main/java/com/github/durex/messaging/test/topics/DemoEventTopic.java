865956da587ae52dbfd7224073643978306aed71
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 11:28:43 +0800

    refactor: remove legacy Redis messaging runtime

--- core/shared/messaging/messaging-api-redis-example/src/main/java/com/github/durex/messaging/test/topics/DemoEventTopic.java
+++
@@ -1,9 +0,0 @@
-package com.github.durex.messaging.test.topics;
-
-import com.github.durex.messaging.api.annotation.Topic;
-import com.github.durex.messaging.api.enums.CodecEnum;
-import com.github.durex.messaging.api.model.EventTopic;
-import com.github.durex.messaging.test.models.DemoEvent;
-
-@Topic(value = TopicNameConstants.DEMO_TOPIC, codec = CodecEnum.MSGPACK)
-public interface DemoEventTopic extends EventTopic<DemoEvent> {}
--- core/shared/messaging/messaging-api-redis/src/main/java/com/github/durex/messaging/api/model/MessageId.java
+++
@@ -1,7 +0,0 @@
-package com.github.durex.messaging.api.model;
-
-import java.io.Serializable;
-
-public interface MessageId<T> extends Comparable<T>, Serializable {
-  T getMessageId();
-}
