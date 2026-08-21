e7653e3468cdef4cca57d56a3dc2f4a92888e677
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 09:38:24 +0800

    refactor: make music repositories runtime neutral

--- core/music/src/spring/java/com/github/durex/music/spring/MusicSpringConfiguration.java
+++ core/music/src/spring/java/com/github/durex/music/spring/MusicSpringConfiguration.java
@@ -1,6 +1,9 @@
 package com.github.durex.music.spring;
 
+import com.github.durex.music.repository.CreatorPlayListRepository;
 import com.github.durex.music.repository.MusicRepository;
+import com.github.durex.music.repository.PlayListMusicRepository;
+import com.github.durex.music.repository.PlayListRepository;
 import com.github.durex.music.service.MusicService;
 import org.jooq.DSLContext;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +15,21 @@ class MusicSpringConfiguration {
   @Bean
   MusicRepository musicRepository(DSLContext dsl) {
     return new MusicRepository(dsl);
+  }
+
+  @Bean
+  PlayListRepository playListRepository(DSLContext dsl) {
+    return new PlayListRepository(dsl);
+  }
+
+  @Bean
+  PlayListMusicRepository playListMusicRepository(DSLContext dsl) {
+    return new PlayListMusicRepository(dsl);
+  }
+
+  @Bean
+  CreatorPlayListRepository creatorPlayListRepository(DSLContext dsl) {
+    return new CreatorPlayListRepository(dsl);
   }
 
   @Bean
