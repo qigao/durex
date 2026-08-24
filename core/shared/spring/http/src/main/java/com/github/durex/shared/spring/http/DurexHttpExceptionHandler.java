d9b3a114966ff9a167fa37c10d7fe807bb39c4d4
Qi Gao <qi.gao2012@gmail.com>
2026-08-25 07:57:45 +0800

    feat: map ApiException at Spring HTTP boundary

---
+++ core/shared/spring/http/src/main/java/com/github/durex/shared/spring/http/DurexHttpExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.github.durex.shared.spring.http;
+
+import com.github.durex.shared.exceptions.ApiException;
+import com.github.durex.shared.exceptions.model.ErrorCode;
+import com.github.durex.shared.exceptions.model.ErrorResponse;
+import com.github.durex.shared.model.RespData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public final class DurexHttpExceptionHandler {
+
+  @ExceptionHandler(ApiException.class)
+  public ResponseEntity<RespData<Void>> handleApiException(ApiException exception) {
+    ErrorResponse error = exception.getErrorResponse();
+    return ResponseEntity.status(statusFor(error.getErrorCode())).body(RespData.of(null, error));
+  }
+
+  private static HttpStatus statusFor(ErrorCode errorCode) {
+    if (errorCode == ErrorCode.ENTITY_NOT_FOUND) {
+      return HttpStatus.NOT_FOUND;
+    }
+    if (errorCode == ErrorCode.EMPTY_PARAM || errorCode == ErrorCode.VALUE_ERROR) {
+      return HttpStatus.BAD_REQUEST;
+    }
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+}
