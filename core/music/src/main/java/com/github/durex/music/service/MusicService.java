cbc5e5360b910a5a86ddb373bb127e762b93af60
Qi Gao <qi.gao2012@gmail.com>
2026-08-25 08:09:31 +0800

    refactor: make Music outcomes explicit

--- core/music/src/main/java/com/github/durex/music/service/MusicService.java
+++ core/music/src/main/java/com/github/durex/music/service/MusicService.java
@@ -1,20 +1,23 @@
 package com.github.durex.music.service;
 
+import static com.github.durex.music.support.EntityConstants.MUSIC_NOT_CREATED;
+import static com.github.durex.music.support.EntityConstants.MUSIC_NOT_DELETED;
 import static com.github.durex.music.support.EntityConstants.MUSIC_NOT_FOUND;
+import static com.github.durex.music.support.EntityConstants.MUSIC_NOT_UPDATED;
+import static com.github.durex.shared.exceptions.model.ErrorCode.DELETE_ERROR;
 import static com.github.durex.shared.exceptions.model.ErrorCode.ENTITY_NOT_FOUND;
+import static com.github.durex.shared.exceptions.model.ErrorCode.SAVE_ERROR;
+import static com.github.durex.shared.exceptions.model.ErrorCode.UPDATE_ERROR;
 
 import com.github.durex.music.model.Music;
 import com.github.durex.music.repository.MusicRepository;
-import com.github.durex.shared.annotation.NullChecker;
-import com.github.durex.shared.annotation.ValueChecker;
 import com.github.durex.shared.exceptions.ApiException;
+import com.github.durex.shared.exceptions.model.ErrorCode;
 import com.github.durex.sqlbuilder.enums.WildCardType;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class MusicService {
   private final MusicRepository repository;
 
@@ -37,11 +40,9 @@ public class MusicService {
   /**
    * get musics by title.
    *
-   * @param title title of the music, must not be null, empty or blank if not found, throw
-   *     ApiException
-   * @return musics, List<{@link Music}>
+   * @param title title of the music
+   * @return matching musics, or an empty list when no music matches
    */
-  @NullChecker
   public List<Music> getMusicsByTitle(String title) {
     return repository.findByTitle(title);
   }
@@ -49,55 +50,53 @@ public class MusicService {
   /**
    * find music by title, title can be like '%title%' or 'title%' or '%title'.
    *
-   * @param title title of the music, must not be null, empty or blank, if not found, throw
-   *     ApiException
+   * @param title title of the music
    * @param wildCardEnum {@link WildCardType}
-   * @return musics, List<{@link Music}>
+   * @return matching musics, or an empty list when no music matches
    */
-  @NullChecker
   public List<Music> getMusicsByTitle(String title, WildCardType wildCardEnum) {
     return repository.findByTitle(title, wildCardEnum);
   }
 
-  @ValueChecker(value = "0", type = Integer.class)
   public Integer createMusic(Music music) {
-    return repository.save(music);
+    return requireAffected(repository.save(music), MUSIC_NOT_CREATED, SAVE_ERROR);
   }
 
-  @NullChecker
   public List<Integer> createMusic(List<Music> musics) {
     return Arrays.stream(repository.save(musics)).boxed().collect(Collectors.toUnmodifiableList());
   }
 
-  @ValueChecker(value = "0", type = Integer.class)
   public Integer updateMusic(Music music) {
-    return repository.update(music);
+    return requireAffected(repository.update(music), MUSIC_NOT_UPDATED, UPDATE_ERROR);
   }
 
-  @NullChecker
   public List<Integer> updateMusic(List<Music> musics) {
     return Arrays.stream(repository.update(musics))
         .boxed()
         .collect(Collectors.toUnmodifiableList());
   }
 
-  @ValueChecker(value = "0", type = Integer.class)
   public Integer deleteMusicById(String id) {
-    return repository.deleteById(id);
+    return requireAffected(repository.deleteById(id), MUSIC_NOT_DELETED, DELETE_ERROR);
   }
 
-  @ValueChecker(value = "0", type = Integer.class)
   public Integer deleteMusicByTitle(String title) {
-    return repository.deleteByTitle(title);
+    return requireAffected(repository.deleteByTitle(title), MUSIC_NOT_DELETED, DELETE_ERROR);
   }
 
-  @ValueChecker(value = "0", type = Integer.class)
   public Integer deleteMusicByTitle(String title, WildCardType wildCardEnum) {
-    return repository.deleteByTitle(title, wildCardEnum);
+    return requireAffected(
+        repository.deleteByTitle(title, wildCardEnum), MUSIC_NOT_DELETED, DELETE_ERROR);
   }
 
-  @ValueChecker(value = "0", type = Integer.class)
   public Integer delete(List<String> musicIds) {
-    return repository.delete(musicIds);
+    return requireAffected(repository.delete(musicIds), MUSIC_NOT_DELETED, DELETE_ERROR);
+  }
+
+  private static int requireAffected(int affected, String message, ErrorCode errorCode) {
+    if (affected == 0) {
+      throw new ApiException(message, errorCode);
+    }
+    return affected;
   }
 }
