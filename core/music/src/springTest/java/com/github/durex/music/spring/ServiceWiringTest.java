7146b1320f5df466795193c7a05fcbbe12a87777
Qi Gao <qi.gao2012@gmail.com>
2026-08-25 08:21:07 +0800

    test: expose transaction wiring diagnostics

--- core/music/src/springTest/java/com/github/durex/music/spring/ServiceWiringTest.java
+++ core/music/src/springTest/java/com/github/durex/music/spring/ServiceWiringTest.java
@@ -1,5 +1,6 @@
 package com.github.durex.music.spring;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,16 +10,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.interceptor.BeanFactoryTransactionAttributeSourceAdvisor;
 
 @SpringBootTest(classes = MusicSpringApplication.class)
 class ServiceWiringTest {
   @Autowired MusicService musicService;
   @Autowired PlaylistService playlistService;
+  @Autowired ApplicationContext context;
 
   @Test
-  void servicesUseSpringWiringAndPlaylistServiceIsTransactionalProxy() {
+  void servicesUseSpringWiringAndPlaylistServiceIsTransactionalProxy() throws Exception {
     assertNotNull(musicService);
     assertNotNull(playlistService);
+    assertFalse(context.getBeansOfType(PlatformTransactionManager.class).isEmpty());
+
+    var advisor = context.getBean(BeanFactoryTransactionAttributeSourceAdvisor.class);
+    var method = PlaylistService.class.getMethod("findPlayList");
+    assertTrue(advisor.getPointcut().getMethodMatcher().matches(method, SpringPlaylistService.class));
     assertTrue(AopUtils.isAopProxy(playlistService));
   }
 }
