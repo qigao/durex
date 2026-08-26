dfbb9395f3ebeb374d95313e2a56d97252511242
Qi Gao <qi.gao2012@gmail.com>
2026-08-26 08:13:06 +0800

    test: require deleted rows absent from normal writes

--- core/music/src/springTest/java/com/github/durex/music/spring/RepositoryWiringTest.java
+++ core/music/src/springTest/java/com/github/durex/music/spring/RepositoryWiringTest.java
@@ -1,5 +1,9 @@
 package com.github.durex.music.spring;
 
+import static com.github.durex.model.tables.QMusic.MUSIC;
+import static com.github.durex.model.tables.QPlaylist.PLAYLIST;
+import static com.github.durex.sqlbuilder.enums.WildCardType.CONTAINS;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -10,9 +14,11 @@ import com.github.durex.music.repository.MusicRepository;
 import com.github.durex.music.repository.PlayListMusicRepository;
 import com.github.durex.music.repository.PlayListRepository;
 import java.util.List;
+import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(classes = MusicSpringApplication.class)
 class RepositoryWiringTest {
@@ -20,6 +26,7 @@ class RepositoryWiringTest {
   @Autowired PlayListRepository playListRepository;
   @Autowired PlayListMusicRepository playListMusicRepository;
   @Autowired CreatorPlayListRepository creatorPlayListRepository;
+  @Autowired DSLContext dsl;
 
   @Test
   void repositoriesAreConstructedBySpringWithoutCdi() {
@@ -36,5 +43,82 @@ class RepositoryWiringTest {
     assertEquals(
         List.of("playlist-1"),
         playListRepository.findAll().stream().map(PlayList::getId).toList());
+  }
+
+  @Test
+  @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+  void normalDeletesIgnoreAlreadyDeletedRowsWithoutRefreshingDeleteTime() {
+    var musicDeleteTimeBefore =
+        dsl.select(MUSIC.DELETE_TIME)
+            .from(MUSIC)
+            .where(MUSIC.ID.eq("music-deleted"))
+            .fetchOne(MUSIC.DELETE_TIME);
+    var playlistDeleteTimeBefore =
+        dsl.select(PLAYLIST.DELETE_TIME)
+            .from(PLAYLIST)
+            .where(PLAYLIST.ID.eq("playlist-deleted"))
+            .fetchOne(PLAYLIST.DELETE_TIME);
+
+    assertAll(
+        () -> assertEquals(0, musicRepository.deleteById("music-deleted")),
+        () -> assertEquals(0, musicRepository.deleteByTitle("Deleted Song")),
+        () -> assertEquals(0, musicRepository.deleteByTitle("Deleted", CONTAINS)),
+        () -> assertEquals(0, musicRepository.delete(List.of("music-deleted"))),
+        () -> assertEquals(0, playListRepository.deleteById("playlist-deleted")),
+        () -> assertEquals(0, playListRepository.deleteById(List.of("playlist-deleted"))),
+        () -> assertEquals(0, playListRepository.deleteByTitle("Deleted Playlist")),
+        () -> assertEquals(0, playListRepository.deleteByTitle("Deleted", CONTAINS)));
+
+    assertEquals(
+        musicDeleteTimeBefore,
+        dsl.select(MUSIC.DELETE_TIME)
+            .from(MUSIC)
+            .where(MUSIC.ID.eq("music-deleted"))
+            .fetchOne(MUSIC.DELETE_TIME));
+    assertEquals(
+        playlistDeleteTimeBefore,
+        dsl.select(PLAYLIST.DELETE_TIME)
+            .from(PLAYLIST)
+            .where(PLAYLIST.ID.eq("playlist-deleted"))
+            .fetchOne(PLAYLIST.DELETE_TIME));
+  }
+
+  @Test
+  @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+  void normalUpdatesIgnoreAlreadyDeletedRows() {
+    String musicTitleBefore =
+        dsl.select(MUSIC.TITLE)
+            .from(MUSIC)
+            .where(MUSIC.ID.eq("music-deleted"))
+            .fetchOne(MUSIC.TITLE);
+    String playlistTitleBefore =
+        dsl.select(PLAYLIST.TITLE)
+            .from(PLAYLIST)
+            .where(PLAYLIST.ID.eq("playlist-deleted"))
+            .fetchOne(PLAYLIST.TITLE);
+
+    var music = new Music();
+    music.setId("music-deleted");
+    music.setTitle("mutated deleted music");
+    var playlist = new PlayList();
+    playlist.setId("playlist-deleted");
+    playlist.setTitle("mutated deleted playlist");
+
+    assertAll(
+        () -> assertEquals(0, musicRepository.update(music)),
+        () -> assertEquals(0, playListRepository.update(playlist)));
+
+    assertEquals(
+        musicTitleBefore,
+        dsl.select(MUSIC.TITLE)
+            .from(MUSIC)
+            .where(MUSIC.ID.eq("music-deleted"))
+            .fetchOne(MUSIC.TITLE));
+    assertEquals(
+        playlistTitleBefore,
+        dsl.select(PLAYLIST.TITLE)
+            .from(PLAYLIST)
+            .where(PLAYLIST.ID.eq("playlist-deleted"))
+            .fetchOne(PLAYLIST.TITLE));
   }
 }
